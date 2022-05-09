2cdfbff5ec0be4cb1b3bf8ebe08abf2bbb272fa6
chinnep <chinnep@miamioh.edu>
2022-05-09 15:02:46 -0400

    twoSum

--- src/1_two-sum/index.spec.js
+++ src/1_two-sum/index.spec.js
@@ -1,25 +1,11 @@
-const { longestCommonPrefix } = require('./index')
+const { twoSum } = require('./index')
 
-describe('longestCommonPrefix', () => {
-  it('should whatever', () => {
-    const input = ['cir', 'car']
-    const expected = 'c'
-    const actual = longestCommonPrefix(input)
-    expect(actual).toBe(expected)
-  })
-
-  it('should return "fl"', () => {
-    const input = ['flower', 'flow', 'flight']
-    const expected = 'fl'
-    const actual = longestCommonPrefix(input)
-    expect(actual).toBe(expected)
-  })
-
-  it('should return ""', () => {
-    // Explanation: There is no common prefix among the input strings.
-    const input = ['dog', 'racecar', 'car']
-    const expected = ''
-    const actual = longestCommonPrefix(input)
-    expect(actual).toBe(expected)
+describe('twoSum', () => {
+  it('should run true', () => {
+    const nums = [2,7,11,15]
+    const target = 9
+    const expected = [0,1]
+    const actual = twoSum(nums, target)
+    expect(actual).toStrictEqual(expected)
   })
 })
